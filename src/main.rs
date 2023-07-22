d12d461eff7ebd9b72959109d7c852b12855a4dc
Bart Massey <bart.massey@gmail.com>
2023-07-22 13:27:51 -0700

    built hw audio pwm setup; does not work yet

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,97 @@
 #![no_main]
 #![no_std]
 
-/// This is a lightly-adapted version of the microbit crate speaker-v2 demo.
+/// This uses code from the microbit crate speaker-v2 demo.
 ///
-/// This code sweeps the output square wave frequency up
-/// from 1Hz at a given sweep rate and PWM duty cycle. When
-/// it reaches a maximum frequency, it holds for a stop time
-/// and then restarts.
+/// This demo plays 8-bit audio — a roughly 1046.5Hz C6 —
+/// out the speaker via high-frequency hardware PWM.
 ///
-/// See the code for constants controlling these parameters.
+/// The chipping rate for the PWM (and thus the "sample
+/// rate" for the audio) is 62.5K samples/second. This means
+/// that the chipping noise is at the Nyquist frequency of
+/// 31.25KHz, which might be beyond what the speaker can do
+/// (it probably isn't) and should be above the likely human
+/// hearing range.
+///
+/// If we wanted a higher sample rate, we'd have to cut down
+/// the bits-per-sample: in the limiting case we'd end up
+/// with one bit per sample sigma-delta at 16MHz, which
+/// might actually be better but without modeling the output
+/// filtering would more likely be garbage.
+
 use panic_halt as _;
 
-use core::cell::RefCell;
-use cortex_m::{asm, interrupt::Mutex};
+use cortex_m::asm;
 use cortex_m_rt::entry;
 use microbit::hal::{
     clocks::Clocks,
     gpio,
-    pac::{self, interrupt},
-    prelude::*,
     pwm,
-    rtc::{Rtc, RtcInterrupt},
-    time::Hertz,
 };
 use microbit::Board;
 
-// Ticks per second.
-const TICK: u32 = 64;
-// Stop frequency in Hertz.
-const STOP_FREQUENCY: u32 = 500;
-// Time to hold at stop frequency in seconds.
-const HOLD_TIME: u32 = 5;
-// Duty cycle in percent. Must be at least 1 and no more than 50.
-const DUTY: u32 = 25;
-
-static RTC: Mutex<RefCell<Option<Rtc<pac::RTC0>>>> = Mutex::new(RefCell::new(None));
-static SPEAKER: Mutex<RefCell<Option<pwm::Pwm<pac::PWM0>>>> = Mutex::new(RefCell::new(None));
+/// Fill `samples` with a full cycle of samples of a sine
+/// wave, with samples quantized to `q`-1 *values* 0..`q`.
+fn sine(samples: &mut [u16], q: u16) {
+    use core::f32::consts::PI;
+    let step = 2.0 * PI / samples.len() as f32;
+    for (i, s) in samples.iter_mut().enumerate() {
+        // Get a value in the range 0.0..1.0.
+        let v = (libm::sinf(i as f32 * step) + 1.0) / 2.0;
+        // Save a value in the range 0..=q.
+        *s = libm::floorf((q - 1) as f32 * v + 0.5) as u16;
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let mut board = Board::take().unwrap();
-    cortex_m::interrupt::free(move |cs| {
-        // NB: The LF CLK pin is used by the speaker
-        let _clocks = Clocks::new(board.CLOCK)
-            .enable_ext_hfosc()
-            .set_lfclk_src_synth()
-            .start_lfclk();
+    // actual frequency with 60 samples is 1041.7 Hz. Can live with it.
+    static mut SAMPLES: [u16; 60] = [0u16; 60];
+    sine(SAMPLES, 256);
 
-        // Set up ticks: TICK = 32768 / (d + 1), so d = 32768 / TICK - 1.
-        let mut rtc = Rtc::new(board.RTC0, 32768 / TICK - 1).unwrap();
-        rtc.enable_counter();
-        rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
-        rtc.enable_event(RtcInterrupt::Tick);
+    let board = Board::take().unwrap();
 
-        *RTC.borrow(cs).borrow_mut() = Some(rtc);
+    // The LF CLK (16MHz) signal is used for PWM.
+    let _clocks = Clocks::new(board.CLOCK)
+        .enable_ext_hfosc()
+        .set_lfclk_src_synth()
+        .start_lfclk();
 
-        let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
-        speaker_pin.set_low().unwrap();
+    // Set up the speaker GPIO pin as an output.
+    let speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::Low);
 
-        // Use the PWM peripheral to generate a waveform for the speaker
-        let speaker = pwm::Pwm::new(board.PWM0);
-        speaker
-            // output the waveform on the speaker pin
-            .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
-            // Use prescale by 16 to achive darker sounds
-            .set_prescaler(pwm::Prescaler::Div16)
-            // Initial frequency
-            .set_period(Hertz(1u32))
-            // Configure for up and down counter mode
-            .set_counter_mode(pwm::CounterMode::UpAndDown)
-            // Set initial maximum duty cycle. This will immediately
-            // be changed in the interrupt handler.
-            .set_max_duty(32767)
-            // enable PWM
-            .enable();
+    // Use the PWM peripheral to generate a waveform for the speaker
+    // The base counter rate for the PWM is 16MHz.
+    // https://jimmywongiot.com/2021/06/01/advanced-pulse-width-modulation-pwm-on-nordic-nrf52-series/
+    // This lets us run at 62500 sps with 256 ticks per sample, since 62500 * 256 = 16M.
+    // We can thus run 8-bit samples at this rate.
+    let speaker = pwm::Pwm::new(board.PWM0);
+    speaker
+        // output the waveform on the speaker pin
+        .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
+        // Prescaler set for 16MHz.
+        .set_prescaler(pwm::Prescaler::Div1)
+        // Configure for up counter mode.
+        .set_counter_mode(pwm::CounterMode::Up)
+        // Read duty cycle values from sequence.
+        .set_load_mode(pwm::LoadMode::Waveform)
+        // Set maximum duty cycle = PWM period in
+        // ticks. 16MHz / 256 = 62_500, our desired sample
+        // rate.
+        .set_max_duty(256)
+        // Set no delay between samples.
+        .set_seq_refresh(pwm::Seq::Seq0, 0)
+        // Set no delay at end of sequence.
+        .set_seq_end_delay(pwm::Seq::Seq0, 0)
+        // Keep playing forever.
+        .loop_inf()
+        // Enable PWM.
+        .enable();
 
-        speaker
-            .set_seq_refresh(pwm::Seq::Seq0, 0)
-            .set_seq_end_delay(pwm::Seq::Seq0, 0);
-
-        // Configure initial duty cycle to 50%. This will
-        // immediately be changed in the interrupt handler.
-        let max_duty = speaker.max_duty();
-        speaker.set_duty_on_common(max_duty / 2);
-
-        *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
-
-        // Configure RTC interrupt
-        unsafe {
-            pac::NVIC::unmask(pac::Interrupt::RTC0);
-        }
-        pac::NVIC::unpend(pac::Interrupt::RTC0);
-    });
+    // Start the sine wave.
+    speaker.load(Some(SAMPLES), None::<&[u16]>, true).unwrap();
 
     loop {
         asm::wfi();
     }
-}
-
-// RTC interrupt, exectued for each RTC tick
-#[interrupt]
-fn RTC0() {
-    static mut FREQUENCY: u32 = 1;
-    /* Enter critical section */
-    cortex_m::interrupt::free(|cs| {
-        /* Borrow devices */
-        if let (Some(speaker), Some(rtc)) = (
-            SPEAKER.borrow(cs).borrow().as_ref(),
-            RTC.borrow(cs).borrow().as_ref(),
-        ) {
-            if *FREQUENCY >= STOP_FREQUENCY + HOLD_TIME * TICK {
-                // Reset the frequency for a new sweep.
-                *FREQUENCY = 1;
-            };
-
-            if *FREQUENCY < STOP_FREQUENCY {
-                // Configure the new frequency, must not be zero.
-                // Will change the max_duty
-                speaker.set_period(Hertz(*FREQUENCY));
-            } else {
-                // Continue at frequency
-                speaker.set_period(Hertz(STOP_FREQUENCY));
-            }
-
-            // Restart the PWM at duty cycle
-            let max_duty = speaker.max_duty() as u32;
-            let duty = DUTY * max_duty / 100;
-            let duty = duty.clamp(1, max_duty / 2);
-            speaker.set_duty_on_common(duty as u16);
-
-            // Clear the RTC interrupt
-            rtc.reset_event(RtcInterrupt::Tick);
-        }
-    });
-    // Increase the frequency
-    *FREQUENCY += 1;
 }

daf719f1a284b2f73be6f7107c75d8603d908fa3
Bart Massey <bart.massey@gmail.com>
2023-07-25 01:26:04 -0700

    added experimental high-drive mode

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 use microbit::Board;
-use microbit::hal::{prelude::*, delay::Delay, gpio::Level};
+use microbit::hal::delay::Delay;
+use microbit::hal::{prelude::*, gpio::{DriveConfig, Level}};
 
 #[entry]
 fn main() -> ! {
     let board = Board::take().unwrap();
     let mut delay = Delay::new(board.SYST);
-    let mut speaker = board.speaker_pin.into_push_pull_output(Level::Low);
+    let mut speaker = board.speaker_pin.into_push_pull_output_drive(
+        Level::Low,
+        DriveConfig::HighDrive0HighDrive1,
+    );
     loop {
         speaker.set_high().unwrap();
         delay.delay_us(500u16);

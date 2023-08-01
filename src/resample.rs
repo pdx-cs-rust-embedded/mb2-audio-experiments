b541704e5fb196d70f85e3de9bad9cef14bbc6a5
Bart Massey <bart.massey@gmail.com>
2023-08-01 13:46:03 -0700

    continued to refactor out iterators

--- src/resample.rs
+++ src/resample.rs
@@ -21,28 +21,37 @@ fn biquad(x: &[f32; 3], y: &[f32; 2], c: &[[f32; 3]; 2]) -> f32 {
     b[0] * x[0] + b[1] * x[1] + b[2] * x[2] - a[1] * y[0] - a[2] * y[1]
 }
 
-// Run a single second-order filter section.
-fn section(
-    x: impl Iterator<Item=f32>,
+struct Section {
     c: [[f32; 3]; 2],
-) -> impl Iterator<Item=f32> {
-    let mut xs = [0.0; 3];
-    let mut ys = [0.0; 2];
-    x.map(move |x0| {
+    xs: [f32; 3],
+    ys: [f32; 2],
+}
+
+impl Section {
+    fn new(c: [[f32; 3]; 2]) -> Self {
+        Self { c, xs: [0.0; 3], ys: [0.0; 2] }
+    }
+
+    fn filter(&mut self, x0: f32) -> f32 {
+        let xs = &mut self.xs;
+        let ys = &mut self.ys;
+        let y0 = biquad(&xs, &ys, &c);
+
         xs[2] = xs[1];
         xs[1] = xs[0];
         xs[0] = x0;
-        let y0 = biquad(&xs, &ys, &c);
+
         ys[1] = ys[0];
         ys[0] = y0;
+
         y0
-    })
+    }
 }
 
 pub struct Upsample16 {
-    i_dest: usize,
+    sections: [Section; 2],
     i_source: usize,
-    source: &'static [f32],
+    source: &'static [u8],
 }
 
 impl Upsample16 {
@@ -50,7 +59,7 @@ impl Upsample16 {
         Upsample16 { i_dest: 0, i_source: 0, source }
     }
 
-    pub fn fill(&mut self, &mut dest) -> bool {
+    pub fn fill(&mut self, dest: &mut [f32]) -> bool {
         for s_out in dest {
             let out = if self.i_out == 0 && self.i_in < self.source.len() {
                 self.i_in += 1;

9af903828087d3abcf91141a0a3a4d37cbcf6e72
Bart Massey <bart.massey@gmail.com>
2025-05-21 13:57:38 +0200

    updated to current

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,22 @@
 use panic_halt as _;
 
 use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::InputPin, digital::OutputPin};
 use microbit::Board;
-use microbit::hal::{prelude::*, delay::Delay, gpio::Level};
+use microbit::hal::{delay::Delay, gpio::Level};
 
 #[entry]
 fn main() -> ! {
     let board = Board::take().unwrap();
     let mut delay = Delay::new(board.SYST);
     let mut speaker = board.speaker_pin.into_push_pull_output(Level::Low);
-    let button = board.buttons.button_a;
+    let mut button = board.buttons.button_a;
     loop {
         if button.is_low().unwrap() {
             speaker.set_high().unwrap();
-            delay.delay_us(500u16);
+            delay.delay_us(500);
             speaker.set_low().unwrap();
-            delay.delay_us(500u16);
+            delay.delay_us(500);
         }
     }
 }
